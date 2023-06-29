f8ad5189fd7a6134500887e0cac52a86aba4387d
qq2530901689@163.com <qq2530901689@163.com>
2023-06-29 11:02:08 +0800

    去掉"秒传"示例和方法

--- ufile/filemanager.py
+++ ufile/filemanager.py
@@ -191,46 +191,6 @@ class FileManager(BaseUFile):
 
         return binarystream
 
-    def uploadhit(self, bucket, key, localfile, header=None):
-        """
-        尝试秒传文件到UFile空间
-
-        :param bucket: string类型，上传空间名称
-        :param key:  string 类型，上传文件在空间中的名称
-        :param localfile: string类型，本地文件名称
-        :param header: dict类型，http 请求header，键值对类型分别为string，比如{'User-Agent': 'Google Chrome'}
-        :return: ret: 如果http状态码为[200, 204, 206]之一则返回None，否则如果服务器返回json信息则返回dict类型，键值对类型分别为string, unicode string类型，否则返回空的dict
-        :return:  ResponseInfo: 响应的具体信息，UCloud UFile 服务器返回信息或者网络链接异常
-        """
-
-        if header is None:
-            header = dict()
-        _check_dict(header)
-        if 'User-Agent' not in header:
-            header['User-Agent'] = config.get_default('user_agent')
-
-        filesize = os.path.getsize(localfile)
-        fileetags = file_etag(localfile, BLOCKSIZE)
-        mimetype = s(mimetype_from_file(localfile))
-
-        # update request header
-        header['Content-Type'] = mimetype
-        header['Content-Length'] = str(0)
-        authorization = self.authorization('post', bucket, key, header)
-        header['Authorization'] = authorization
-
-        # parameter
-        params = {'Hash': fileetags,
-                  'FileName': key,
-                  'FileSize': filesize}
-
-        url = ufile_uploadhit_url(bucket, upload_suffix=self.__upload_suffix)
-
-        logger.info('start upload hit localfile {0} as {1} in bucket {2}'.format(localfile, key, bucket))
-        logger.info('request url: {0}'.format(url))
-
-        return _uploadhit_file(url, header, params)
-
     def download_file(self, bucket, key, localfile, isprivate=True, expires=None, content_range=None, header=None):
         """
         下载UFile文件并且保存为本地文件
